4710df0a1f536e8513068343a1d49908c3fbd885
nakaikaz <nakaikaz@gmail.com>
2016-02-24 16:51:32 +0900

    2016-02-24

--- js/controllers/AppController.js
+++ js/controllers/AppController.js
@@ -1,40 +1,43 @@
 app.controller('AppController', ['$scope', '$rootScope', '$location', 'Auth', function($scope, $rootScope, $location, Auth){
-	$scope.loading = false;
-	$scope.loginError = false;
-	$scope.signupError = false;
-	$scope.login = {};
-	$scope.signup = {};
-	$scope.signUp = function(){
-		if($scope.signupForm.$invalid){
-			return;
-		}
-		Auth.post('signup', {user: $scope.signup}).then(function(response){
-			if(response.status){
-				$location.path('reports');
-			}else{
-				$scope.signupError = true;
-			}
-		}, function(err){
-			console.log(err);
-		});
-	};
-	$scope.doLogin = function(){
-		Auth.post('login', {user: $scope.login}).then(function(response){
-			if(response.status){
-				$location.path('reports');
-			}else{
-				$scope.loginError = true;
-			}
-		}, function(err){
-			console.log(err);
-		});
-	}
-	$scope.logout = function(){
-		Auth.get('logout').then(function(response){
-			$location.path('login');
-		});
-	};
-	$scope.$on('loading', function(event, args){
-		$scope.loading = args;
-	});
+    $scope.loading = false;
+    $scope.loginError = false;
+    $scope.signupError = false;
+    $scope.login = {};
+    $scope.signup = {};
+    $scope.preSignUp = function(){
+
+    };
+    $scope.signUp = function(){
+        if($scope.signupForm.$invalid){
+            return;
+        }
+        Auth.post('signup', {user: $scope.signup}).then(function(response){
+            if(response.status){
+                $location.path('reports');
+            }else{
+                $scope.signupError = true;
+            }
+        }, function(err){
+            console.log(err);
+        });
+    };
+    $scope.doLogin = function(){
+        Auth.post('login', {user: $scope.login}).then(function(response){
+            if(response.status){
+                $location.path('reports');
+            }else{
+                $scope.loginError = true;
+            }
+        }, function(err){
+            console.log(err);
+        });
+    }
+    $scope.logout = function(){
+        Auth.get('logout').then(function(response){
+            $location.path('login');
+        });
+    };
+    $scope.$on('loading', function(event, args){
+        $scope.loading = args;
+    });
 }]);
